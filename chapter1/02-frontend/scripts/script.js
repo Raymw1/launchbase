4c84e76cf1af8f77f64c44de748baf2f2a56f1a5
Raymw1 <rayan.rmw@gmail.com>
2021-07-15 17:25:55 -0300

    Front end of the website done

--- chapter1/02-frontend/scripts/script.js
+++ chapter1/02-frontend/scripts/script.js
@@ -3,10 +3,14 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
+    const videoId = card.getAttribute("id");
+    modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
     modalOverlay.classList.add("active");
   });
 });
 
 document.querySelector(".modal-overlay a.close-modal").addEventListener("click", () => {
+    modalOverlay.querySelector("iframe").src = '';
     modalOverlay.classList.remove("active");
-  });
+});
+
