0c15a7a9ecb0fed67bb3bd345b09fa6b1cce82cf
Raymw1 <rayan.rmw@gmail.com>
2021-07-18 13:46:06 -0300

    Adding dynamic data

---
+++ chapter1/03-backend/data.js
@@ -0,0 +1,44 @@
+module.exports = [
+//   {
+//     id: "TODO",
+//     title: "TODO",
+//     duration: "TODO",
+//     price: "TODO"
+//   },
+  {
+    id: "bk4abNFLDE8",
+    title: "Git e GitHub - o que é isso? | Jakeline",
+    duration: "20 min",
+    price: "Free"
+  },
+  {
+    id: "q_ppL_sU6qU",
+    title: "CSS - Como eu penso em CSS? | Mayk",
+    duration: "20 min",
+    price: "Free"
+  },
+  {
+    id: "X6klpqubyBw",
+    title: "JavaScript - Maneiras de criar objetos | Mayk",
+    duration: "38 min",
+    price: "Free"
+  },
+  {
+    id: "M7CkkeECodk",
+    title: "Ubuntu - Comandos básicos para iniciantes no terminal | Jakeline",
+    duration: "26 min",
+    price: "Free"
+  },
+  {
+    id: "RF0nS-EM2Ag",
+    title: "Funções - aprendendo além do console.log() | Mayk",
+    duration: "15 min",
+    price: "Free"
+  },
+  {
+    id: "E28J23gCBIs",
+    title: "Git - Principais comandos para terminal | Jakeline",
+    duration: "25 min",
+    price: "Free"
+  }
+];
--- chapter1/03-backend/server.js
+++ chapter1/03-backend/server.js
@@ -1,25 +1,37 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const videos = require("./data");
 
 const server = express();
 
-server.set('view engine', 'njk')
-nunjucks.configure('views', {
-    express: server,
-    noCache: true
-})
+server.set("view engine", "njk");
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,    // HTML in data
+  noCache: true,
+});
 server.use(express.static("public"));
 
-server.get("/", function(req, res) {
-    return res.render("about");
+server.get("/", function (req, res) {
+  const about = {
+    avatar_url: "https://github.com/raymw1.png",
+    name: "Rayan Wilbert",
+    role: "Desenvolvedor FullStack",
+    description: `Desenvolvedor FullStack de 17 anos, procurando desafios que melhore sua rotina e também a dos outros. Aluno da <a href="https://rocketseat.com.br/" target="_blank">RocketSeat</a>`,
+    links: [
+      { name: "Gitub", url: "https://github.com/Raymw1/" },
+      { name: "Instagram", url: "https://www.instagram.com/ray.coding/" },
+      { name: "LinkedIn", url: "https://www.linkedin.com/in/rayanwilbert/" }
+    ]
+  };
+  return res.render("about", { about });
 });
 
-server.get("/classes", function(req, res) {
-    return res.render("classes");
+server.get("/classes", function (req, res) {
+  return res.render("classes", { videos });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Go to http://127.0.0.1:${PORT}`);
-})
-
+  console.log(`Go to http://127.0.0.1:${PORT}`);
+});
