f6518ee30730ba04c16434c86a62b75ea87d3d07
Raymw1 <rayan.rmw@gmail.com>
2021-07-19 21:14:52 -0300

    Adding toggle text in hide function - Foodfy

--- challenges/challenges03/challenge4-foodfy/public/scripts/hide.js
+++ challenges/challenges03/challenge4-foodfy/public/scripts/hide.js
@@ -4,8 +4,10 @@ hideBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         const elementToHide = btn.parentElement.nextElementSibling;
         if (elementToHide.classList.contains("hidden")) {
+            btn.innerText = "ESCONDER";
             elementToHide.classList.remove("hidden");
         }   else {
+            btn.innerText = "MOSTRAR";
             elementToHide.classList.add("hidden");
         }
     })
