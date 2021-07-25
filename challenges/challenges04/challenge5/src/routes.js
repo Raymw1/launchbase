03b61239c41c469d493537bf45601775a2ab4cb8
Raymw1 <rayan.rmw@gmail.com>
2021-07-25 16:53:34 -0300

    Challenge 4-5 done

---
+++ challenges/challenges04/challenge5/src/routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const teachers = require("./controllers/teacherController");
+const routes = express.Router();
+
+routes.get("/", function (req, res) {
+  return res.redirect("/teachers");
+});
+
+routes.get("/teachers", function (req, res) {
+  return res.render("teachers/index");
+});
+
+routes.get("/teachers/create", teachers.create);
+
+routes.get("/teachers/:id", teachers.show);
+
+routes.get("/teachers/:id/edit", teachers.edit);
+
+routes.post("/teachers", teachers.post);
+
+routes.put("/teachers", teachers.put);
+
+routes.delete("/teachers", teachers.delete);
+
+module.exports = routes;
