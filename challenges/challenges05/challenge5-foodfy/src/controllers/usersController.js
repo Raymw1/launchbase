54a1ce2b0d30542f387f72084f2cd6d5655837b2
Raymw1 <rayan.rmw@gmail.com>
2021-08-03 17:01:35 -0300

    Challenge 5-5: pagination added

--- challenges/challenges05/challenge5-foodfy/src/controllers/usersController.js
+++ challenges/challenges05/challenge5-foodfy/src/controllers/usersController.js
@@ -7,25 +7,34 @@ module.exports = {
     Recipe.all(function (recipes) {
       mostViewedRecipes = [...recipes];
       while (mostViewedRecipes.length > 6) {
-        mostViewedRecipes.pop()
+        mostViewedRecipes.pop();
       }
       return res.render("users/index", { recipes: mostViewedRecipes });
-    })
+    });
   },
   about(req, res) {
     return res.render("users/about");
   },
   recipes(req, res) {
-    const { filter } = req.query;
-    if (filter) {
-      Recipe.findBy(filter, function (recipes) {
-        return res.render("users/recipes", { recipes, filter });
-      })
-    } else {
-      Recipe.all(function (recipes) {
-        return res.render("users/recipes", { recipes });
-      })
-    }
+    let { filter, page, limit } = req.query;
+    page = page || 1;
+    limit = limit || 2;
+    let offset = limit * (page - 1);
+    const params = {
+      filter,
+      page,
+      limit,
+      offset,
+      callback(recipes) {
+        let total = recipes[0]?.total || 0
+        const pagination = {
+          total: Math.ceil(total / limit),
+          page,
+        };
+        return res.render("users/recipes", { recipes, filter, pagination });
+      },
+    };
+    Recipe.paginate(params)
   },
   recipe(req, res) {
     Recipe.find(req.params.id, function (recipe) {
@@ -33,11 +42,11 @@ module.exports = {
         return res.send("Recipe not found");
       }
       return res.render("users/recipe", { recipe });
-    })
+    });
   },
   chefs(req, res) {
     Chef.all(function (chefs) {
-      return res.render("users/chefs", {chefs})
-    })
-  }
-}
+      return res.render("users/chefs", { chefs });
+    });
+  },
+};
--- challenges/challenges05/challenge5-foodfy/src/model/Recipe.js
+++ challenges/challenges05/challenge5-foodfy/src/model/Recipe.js
@@ -25,6 +25,26 @@ module.exports = {
       callback(results.rows);      
     })
   },
+  paginate(params) {
+    const { filter, limit, offset, callback } = params;
+    let query = "",
+      filterQuery="",
+      totalQuery="(SELECT count(*) FROM recipes) AS total";
+    if (filter) {
+      filterQuery = `WHERE recipes.title ILIKE '%${filter}%'`;
+      totalQuery = `(SELECT count(*) FROM recipes ${filterQuery}) AS total`;
+    }
+    query = `
+    SELECT recipes.*, chefs.name as chef_name, ${totalQuery} FROM recipes 
+    LEFT JOIN chefs ON (chefs.id = recipes.chef_id)
+    ${filterQuery}
+    GROUP BY recipes.id, chefs.name LIMIT $1 OFFSET $2
+    ;`
+    db.query(query, [limit, offset], function (err, results) {
+      if (err) throw `Database error! ${err}`;
+      callback(results.rows);
+    })
+  },
   create(data, callback) {
     const query = `
     INSERT INTO recipes (
