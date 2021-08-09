a5529ea41a8cbab3cb5ae5ba90a77e907c2299c9
Raymw1 <rayan.rmw@gmail.com>
2021-08-08 22:01:12 -0300

    Chapter 8-1: searching done

--- chapter3/08-listing-products/src/app/models/Product.js
+++ chapter3/08-listing-products/src/app/models/Product.js
@@ -59,5 +59,20 @@ module.exports = {
   },
   files(id) {
     return db.query("SELECT * FROM files WHERE product_id = $1;", [id]);
+  },
+  search(params) {
+    const { filter, category } = params;
+    let query = '',
+      filterQuery = `WHERE`;
+    if(category) {
+      filterQuery = `${filterQuery} products.category_id = ${category} AND `
+    }
+    filterQuery = `${filterQuery} products.name ILIKE '%${filter}%' OR products.description ILIKE '%${filter}%'`
+    query = `SELECT products.*, categories.name AS category_name FROM products
+    LEFT JOIN categories ON (categories.id = products.category_id)
+    ${filterQuery}
+    GROUP BY products.id, categories.name
+    ;`
+    return db.query(query);
   }
 };
--- chapter3/08-listing-products/src/routes.js
+++ chapter3/08-listing-products/src/routes.js
@@ -2,15 +2,23 @@ const express = require("express");
 const routes = express.Router();
 const ProductController = require("./app/controllers/ProductController");
 const HomeController = require("./app/controllers/HomeController");
+const SearchController = require("./app/controllers/SearchController");
 const multer = require("./app/middlewares/multer");
 
+/* ============= HOME ============= */
 routes.get("/", HomeController.index);
+
+/* ============= SEARCH ============= */
+routes.get("/products/search", SearchController.index)
+
+/* ============= PRODUCTS ============= */
 routes.get("/products/create", ProductController.create);
 routes.get("/products/:id", ProductController.show)
 routes.get("/products/:id/edit", ProductController.edit)
 routes.post("/products", multer.array("photos", 6), ProductController.post);
 routes.put("/products", multer.array("photos", 6), ProductController.put);
 routes.delete("/products", ProductController.delete);
+
 
 /* ============= ALIAS ============= */
 routes.get("/ads/create", function (req, res) {
