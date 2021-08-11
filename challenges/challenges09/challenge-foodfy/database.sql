09bdbda7d1b02cbab5b41468c30bec78a4ed23e4
Raymw1 <rayan.rmw@gmail.com>
2021-08-11 18:02:41 -0300

    Challenge 9 - foodfy: Adding users table

---
+++ challenges/challenges09/challenge-foodfy/database.sql
@@ -0,0 +1,44 @@
+-- CREATE TABLE "files" (
+--   "id" SERIAL PRIMARY KEY,
+--   "name" text,
+--   "path" text NOT NULL,
+--   "product_id" integer
+-- );
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "reset_token" text,
+  "reset_token_expires" text,
+  "is_admin" boolean DEFAULT false,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+------------- FOREIGN KEYS -------------
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+------------- PROCEDURE -------------
+
+-- CREATE FUNCTION trigger_set_timestamp()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--   NEW.updated_at = NOW();
+--   RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+------------- AUTO UPDATED_AT PRODUCTS -------------
+-- CREATE TRIGGER set_timestamp
+-- BEFORE UPDATE ON products
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE trigger_set_timestamp();
+
+------------- AUTO UPDATED_AT USERS -------------
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
