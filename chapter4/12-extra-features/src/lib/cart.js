6bd6069299aa9d2b610023616c7e1623d4a56d17
Raymw1 <rayan.rmw@gmail.com>
2021-08-26 18:52:12 -0300

    Chapter 9-2: Remove product of cart function done

--- chapter4/12-extra-features/src/lib/cart.js
+++ chapter4/12-extra-features/src/lib/cart.js
@@ -27,21 +27,35 @@ const Cart = {
         price: 0,
         formattedPrice: 0
       }
-      this.items.push(inCart)
+      this.items.push(inCart);
     }
 
-    if (inCart.quantity >= product.quantity) return this
+    if (inCart.quantity >= product.quantity) return this;
 
     inCart.quantity++;
     inCart.price = inCart.product.price * inCart.quantity;
     inCart.formattedPrice = formatPrice(inCart.price);
-    this.total.quantity++
+    this.total.quantity++;
     this.total.price += inCart.product.price;
     this.total.formattedPrice = formatPrice(this.total.price);
     return this
   },
   removeOne(productId) {
+    const inCart = this.items.find(item => item.product.id == productId);
+    if (!inCart) return this;
+    inCart.quantity--;
+    inCart.price = inCart.product.price - inCart.quantity;
+    inCart.formattedPrice = formatPrice(inCart.price);
 
+    this.total.quantity--;
+    this.total.price -= inCart.product.price;
+    this.total.formattedPrice = formatPrice(this.total.price);
+    if (inCart.quantity <= 0) {
+      this.items = this.items.filter(item => item.product.id != productId)
+      // const itemIndex = this.items.indexOf(inCart);
+      // this.items.splice(itemIndex, 1);
+    }
+    return this
   },
   delete(productId) {
 
@@ -62,8 +76,10 @@ const product2 = {
 
 let a = Cart.init().addOne(product)
 a = Cart.init(a).addOne(product)
+console.log(a);
 a = Cart.init(a).addOne(product2)
-a = Cart.init(a).addOne(product2)
+console.log(a);
+a = Cart.init(a).removeOne(product2.id)
 console.log(a);
 
 module.exports = Cart;
