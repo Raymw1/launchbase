201d811c94486aa9e6c2150ef05178326d239943
Raymw1 <rayan.rmw@gmail.com>
2021-08-25 21:59:24 -0300

    Challenge 10 - foodfy: Updating database.sql

--- challenges/challenges10/challenge-foodfy/database.sql
+++ challenges/challenges10/challenge-foodfy/database.sql
@@ -1,9 +1,16 @@
--- CREATE TABLE "files" (
---   "id" SERIAL PRIMARY KEY,
---   "name" text,
---   "path" text NOT NULL,
---   "product_id" integer
--- );
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+DROP DATABASE IF EXISTS foodfy;
+CREATE DATABASE foodfy;
+
+------------- RESET TABLES ---------------- 
+-- TRUNCATE chefs RESTART IDENTITY CASCADE;
+-- TRUNCATE files RESTART IDENTITY CASCADE;
+-- TRUNCATE recipe_files RESTART IDENTITY CASCADE;
+-- TRUNCATE recipes RESTART IDENTITY CASCADE;
+-- TRUNCATE session RESTART IDENTITY CASCADE;
+-- TRUNCATE users RESTART IDENTITY CASCADE;
 
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
@@ -24,25 +31,51 @@ CREATE TABLE "chefs" (
   "avatar" integer
 );
 
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "recipes" (
+    "id" SERIAL NOT NULL,
+    "chef_id" integer NOT NULL,
+    "title" text NOT NULL,
+    "ingredients" text[] NOT NULL,
+    "preparation" text[] NOT NULL,
+    "information" text NOT NULL,
+    "created_at" timestamp without time zone DEFAULT now() NOT NULL,
+    "updated_at" timestamp without time zone DEFAULT now() NOT NULL,
+    "user_id" integer
+);
+
+CREATE TABLE "recipe_files" (
+  "id" SERIAL PRIMARY KEY,
+  "recipe_id" integer NOT NULL,
+  "file_id" integer NOT NULL
+);
+
 ------------- FOREIGN KEYS -------------
 
 ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
 
-------------- PROCEDURE -------------
+----------- PROCEDURE -------------
 
--- CREATE FUNCTION trigger_set_timestamp()
--- RETURNS TRIGGER AS $$
--- BEGIN
---   NEW.updated_at = NOW();
---   RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-------------- AUTO UPDATED_AT PRODUCTS -------------
--- CREATE TRIGGER set_timestamp
--- BEFORE UPDATE ON products
--- FOR EACH ROW
--- EXECUTE PROCEDURE trigger_set_timestamp();
+----------- AUTO UPDATED_AT RECIPES -------------
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON recipes
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
 
 ------------- AUTO UPDATED_AT USERS -------------
 CREATE TRIGGER set_timestamp
